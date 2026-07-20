a149b30f2719864cb13ee88d2fab6def7a2a9ab5
Geppie3 <gerbenrohof@gmail.com>
2026-07-20 16:26:40 +0200

    1.1.0
    1.1.0
    Added configurable Ender Staff bonuses:
    Ender spell power
    Mana regeneration
    Maximum mana
    Added a revised custom 3D Ender Staff model.
    Added updated staff textures, including the Eye, platform, hilt, holder, handle, and shaft textures.

--- src/main/java/com/example/enderstaves/EnderStaves.java
+++ src/main/java/com/example/enderstaves/EnderStaves.java
@@ -1,15 +1,25 @@
 package com.example.enderstaves;
 
+import com.example.enderstaves.config.EnderStavesConfig;
 import com.example.enderstaves.registry.ModItems;
+
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(EnderStaves.MOD_ID)
 public final class EnderStaves {
     public static final String MOD_ID = "enderstaves";
 
     public EnderStaves() {
+        ModLoadingContext.get().registerConfig(
+                ModConfig.Type.COMMON,
+                EnderStavesConfig.SPEC,
+                "enderstaves-common.toml"
+        );
+
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModItems.register(modBus);
         modBus.addListener(ModItems::addToCreativeTabs);
